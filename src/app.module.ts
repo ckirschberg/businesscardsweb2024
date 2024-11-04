b2ca5ca41022a77ca9c83857e400f387edee52cf
Christian Kirschberg <ckirschberg@gmail.com>
2024-11-04 12:29:39 +0100

    auth no db yet

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { CatsController } from './cats/cats.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { BusinessCardsModule } from './business-cards/business-cards.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/business-cards-web2024'),
     CatsModule,
     BusinessCardsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
